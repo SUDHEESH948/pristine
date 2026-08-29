1fe56175a4e72d004ab0e09361d1c2a2aaf6246a
Sudheesh KS <Kssudheesh818@gmail.com>
2026-08-29 14:15:47 +0530

    Update home page

--- PRIST/src/pages/Home.jsx
+++ PRIST/src/pages/Home.jsx
@@ -1,17 +1,11 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Benefit from "../components/OurImpact";
 import Aboute from "../components/WhyPristine";
 import Value from "../components/Values";
-import ContactFooter from "../components/Footer";
 
 function Home() {
   return (
     <main className="min-h-screen overflow-hidden bg-[#071a36] text-white">
-
-      {/* Navigation */}
-      <Navbar />
-
       {/* Hero Banner */}
       <Hero />
 
@@ -27,9 +21,6 @@ function Home() {
       <section className="premium-section">
         <Value />
       </section>
-
-      
-
     </main>
   );
 }
